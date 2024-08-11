f0658f01f29c9c01c66bd981650e1ce4c8b4b98f
Wenchen Lie <438171766@qq.com>
2024-08-12 01:25:39 +0800

    改材料名

---
+++ OSMatTest.py
@@ -0,0 +1,53 @@
+import numpy as np
+import openseespy.opensees as ops
+import matplotlib.pyplot as plt
+
+
+def material_test(
+        strain: list[float],
+        mat_type: str,
+        para: list,
+    ) -> list[float]:
+    ops.wipe()
+    ops.uniaxialMaterial(mat_type, 1, *para)
+    ops.testUniaxialMaterial(1)
+    stess = []
+    for val in strain:
+        ops.setStrain(val)
+        stess.append(ops.getStress())
+    return stess
+
+
+def generate_path(disp_level: list, n: int=200):
+    u = []
+    for i, disp in enumerate(disp_level[1:]):
+        for j in range(n):
+            ui = disp_level[i] + (disp_level[i + 1] - disp_level[i]) * j / n
+            u.append(ui)
+    else:
+        u.append(disp_level[-1])
+    return u
+
+
+
+
+if __name__ == "__main__":
+
+    # data = np.loadtxt('SMA6.txt')
+    u = generate_path([0, 10, -10, 20, -20, 30, -30, 40, -40, 55, -55, 0])  # 目标位移幅值点
+
+
+    mat = 'TSB'
+    Fslip = 30
+    k = 300
+    ugap = 20
+    N = 1
+    Fy, k1, k2, beta, ubear, kbear = 40, 50, 2, 0.4, 50, 30
+    paras_a = [Fslip, k, ugap, N, Fy, k1, k2, beta, ubear, kbear]
+
+    F = material_test(u, mat, paras_a)
+    F = np.array(F)
+    plt.plot(u, F, label='base')
+    plt.ylim(-150, 150)
+    plt.legend()
+    plt.show()
