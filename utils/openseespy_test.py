a521279a3177a9dfef0a022f7788c4202aae5657
Wenchen Lie <438171766@qq.com>
2024-09-20 12:53:25 +0800

    增加测试pyd文件

---
+++ utils/openseespy_test.py
@@ -0,0 +1,33 @@
+import numpy as np
+import bin.opensees as ops
+import matplotlib.pyplot as plt
+
+
+def material_test(
+        strain: list[float],
+        mat_type: str,
+        para: list,
+    ) -> tuple[list, list]:
+    ops.wipe()
+    ops.uniaxialMaterial(mat_type, 1, *para)
+    ops.testUniaxialMaterial(1)
+
+    stess = []
+    tangent = []
+    for val in strain:
+        ops.setStrain(val)
+        stess.append(ops.getStress())
+        tangent.append(ops.getTangent())
+    return stess, tangent
+
+
+def generate_path(disp_level: list, n: int=200):
+    u = []
+    for i, disp in enumerate(disp_level[1:]):
+        for j in range(n):
+            ui = disp_level[i] + (disp_level[i + 1] - disp_level[i]) * j / n
+            u.append(ui)
+    else:
+        u.append(disp_level[-1])
+    return u
+
